0d79e4b318125e28ab3bfe3426808642581b9a15
Alenmnoian4 <alenmnoian4@gmail.com>
2022-11-16 00:02:01 -0500

    git commit -m "commit"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const app = require('liquid-express-views')(express())
+const app = require(express())
 const port = 3000
 const drinks = require('./models/drinks.js')
 
