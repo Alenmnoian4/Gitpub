ffe2c08bb997d0b7f856f2cc3ff986788e10f447
Alenmnoian4 <alenmnoian4@gmail.com>
2022-11-15 23:59:36 -0500

    git commit -m "commit"

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const app = require('liquid-express-views')(express())
+const port = 3000
+const drinks = require('./models/drinks.js')
+
+const choices = (req) => {
+    console.log(`${req.method} ${req.url}`)
+    console.log(req.params)
+    
+}
+
+app.get('/', (req, res) => {
+    choices(req)
+    res.send('Welcome to the Gitpub App!')
+})
+
+app.get('/drinks', (req, res) => {
+    res.render('index', {drink: drinks})
+})
+
+app.get('drinks/:id', (req, res) => {
+    res.render('show', {drink: drinks[req.params.id]})
+})
+
+app.listen(port, () => {
+    console.log('running', port)
+})
